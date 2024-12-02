49236f380721707b05477856fa13e32c9f8fb685
Andrew Wason <rectalogic@rectalogic.com>
2024-12-02 17:51:36 -0500

    Add github pages deploy. Make app work client side.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,27 @@
-import { agent } from "@/lib/api";
+'use client';
 
-export default async function Homepage() {
-  const feeds = await agent.app.bsky.unspecced.getPopularFeedGenerators({
-    limit: 10,
-  });
+import { useEffect, useState } from 'react';
+import { agent } from "@/lib/api";
+import { AppBskyUnspeccedGetPopularFeedGenerators } from "@atproto/api";
+
+export default function Homepage() {
+  const [feeds, setFeeds] = useState<AppBskyUnspeccedGetPopularFeedGenerators.Response | null>(null);
+  useEffect(() => {
+    const fetchFeeds = async () => {
+      const result = await agent.app.bsky.unspecced.getPopularFeedGenerators({
+        limit: 10,
+      });
+      setFeeds(result);
+    };
+
+    fetchFeeds();
+  }, []);
 
   return (
     <div className="container mx-auto">
       <h1 className="font-bold text-xl my-4">Top Feeds</h1>
       <ul>
-        {feeds.data.feeds.map((feed) => (
+        {feeds?.data.feeds.map((feed) => (
           <li key={feed.displayName}>{feed.displayName}</li>
         ))}
       </ul>
