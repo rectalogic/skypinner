1d66600cc34bc30ded0ddf6357c375afbf3f3986
Andrew Wason <rectalogic@rectalogic.com>
2024-12-03 12:46:49 -0500

    prettier. use SWR for client side fetches.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,31 @@
-'use client';
+"use client";
 
-import { useEffect, useState } from 'react';
+import useSWR from "swr";
 import { agent } from "@/lib/api";
-import { AppBskyUnspeccedGetPopularFeedGenerators } from "@atproto/api";
 
 export default function Homepage() {
-  const [feeds, setFeeds] = useState<AppBskyUnspeccedGetPopularFeedGenerators.Response | null>(null);
-  useEffect(() => {
-    const fetchFeeds = async () => {
-      const result = await agent.app.bsky.unspecced.getPopularFeedGenerators({
-        limit: 10,
-      });
-      setFeeds(result);
-    };
+  const { data, error, isLoading } = useSWR("getPopularFeedGenerators", () =>
+    agent.app.bsky.unspecced.getPopularFeedGenerators({
+      limit: 10,
+    })
+  );
 
-    fetchFeeds();
-  }, []);
+  let contents;
+  if (error) contents = <div>failed to load</div>;
+  else if (isLoading) contents = <div>loading...</div>;
+  else
+    contents = (
+      <ul>
+        {data?.data.feeds.map((feed) => (
+          <li key={feed.displayName}>{feed.displayName}</li>
+        ))}
+      </ul>
+    );
 
   return (
     <div className="container mx-auto">
       <h1 className="font-bold text-xl my-4">Top Feeds</h1>
-      <ul>
-        {feeds?.data.feeds.map((feed) => (
-          <li key={feed.displayName}>{feed.displayName}</li>
-        ))}
-      </ul>
+      {contents}
     </div>
   );
 }
